9180caa74ed7aa58006d2acb31ba58394a9f75f5
MGauza <MGauza@hemmersbach.com>
2019-04-16 15:32:55 +0200

    fallowers frgment in progress

--- app/src/main/java/pl/sjmprofil/animaltinder/adapters/FollowersRecyclerViewAdapter.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/adapters/FollowersRecyclerViewAdapter.kt
@@ -1,4 +1,39 @@
 package pl.sjmprofil.animaltinder.adapters
 
-class FollowersRecyclerViewAdapter {
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pl.sjmprofil.animaltinder.R
+import pl.sjmprofil.animaltinder.models.User
+
+class FollowersRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val TAG = "FollowersRecyclerView"
+    }
+
+    lateinit var fallowersList: MutableList<User>
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.cardview_followers, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    fun updateList(list: MutableList<User>){
+        fallowersList.clear()
+        fallowersList.addAll(list)
+        notifyDataSetChanged()
+        Log.d(TAG, "list updated")
+    }
+
+    override fun getItemCount() = fallowersList.size
+
+    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
 }
--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/followers/FollowersFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/followers/FollowersFragment.kt
@@ -1,4 +1,17 @@
 package pl.sjmprofil.animaltinder.fragments.followers
 
-class FollowersFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pl.sjmprofil.animaltinder.R
+
+class FollowersFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.layout_fallowers_fragment, container, false)
+    }
+
+
+
 }
