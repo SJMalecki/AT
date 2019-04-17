6b7ae0cc5deed9c80b78e1ac0a3f7ce63910c68d
Malecki Sebastian <sebastian.malecki@hemmersbach.com>
2019-04-17 17:48:36 +0200

    Added image + button + email textView - data binding

--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/userprofile/UserProfileFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/userprofile/UserProfileFragment.kt
@@ -1,30 +1,24 @@
 package pl.sjmprofil.animaltinder.fragments.userprofile
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.user_profile_fragment_layout.*
 import pl.sjmprofil.animaltinder.R
+import pl.sjmprofil.animaltinder.databinding.UserProfileFragmentLayoutBinding
+import pl.sjmprofil.animaltinder.models.Advert
 
 class UserProfileFragment : Fragment() {
 
+    private lateinit var bindUserProfileFragment: UserProfileFragmentLayoutBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.user_profile_fragment_layout, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        Picasso
-            .get()
-            .load("http://sjmprofil.pl/imgjson/18.jpg")
-            .centerCrop()
-            .placeholder(R.drawable.ic_hourglass_empty_black_24dp)
-            .error(R.drawable.ic_add_a_photo_black_24dp)
-            .resize(500,500)
-            .into(image_view_user_profile_fragment)
+        bindUserProfileFragment =
+                DataBindingUtil.inflate(inflater, R.layout.user_profile_fragment_layout, container, false)
+        val advert = Advert("nameTest", "surnameTest", "http://sjmprofil.pl/imgjson/18.jpg")
+        bindUserProfileFragment.advert = advert
+        return bindUserProfileFragment.root
     }
 }
