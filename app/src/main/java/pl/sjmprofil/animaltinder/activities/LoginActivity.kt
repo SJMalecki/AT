3132f5a9922c8a7011071288e74d18d44bac157f
Tomek <Tomasz.Lukaszewski@hemmersbach.com>
2019-04-17 08:52:05 +0200

    commit 1

--- app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
@@ -1,4 +1,14 @@
 package pl.sjmprofil.animaltinder.activities
 
-class LoginActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pl.sjmprofil.animaltinder.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
 }
