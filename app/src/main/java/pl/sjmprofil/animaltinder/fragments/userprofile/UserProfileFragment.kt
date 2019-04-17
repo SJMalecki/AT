5624877f43d278def61223eff7ef4cfbaf040c30
Malecki Sebastian <sebastian.malecki@hemmersbach.com>
2019-04-17 14:20:31 +0200

    Picasso test, nav drawer pic change

--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/userprofile/UserProfileFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/userprofile/UserProfileFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.user_profile_fragment_layout.*
 import pl.sjmprofil.animaltinder.R
 
 class UserProfileFragment : Fragment() {
@@ -13,5 +15,16 @@ class UserProfileFragment : Fragment() {
         return inflater.inflate(R.layout.user_profile_fragment_layout, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        Picasso
+            .get()
+            .load("http://sjmprofil.pl/imgjson/18.jpg")
+            .centerCrop()
+            .placeholder(R.drawable.ic_hourglass_empty_black_24dp)
+            .error(R.drawable.ic_add_a_photo_black_24dp)
+            .resize(500,500)
+            .into(image_view_user_profile_fragment)
+    }
 }
