9d8a62afffe1e5485daab9aca3a26b019bc06f34
Tomek <Tomasz.Lukaszewski@hemmersbach.com>
2019-04-17 15:22:01 +0200

    commit 2

--- app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
@@ -2,6 +2,7 @@ package pl.sjmprofil.animaltinder.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 import pl.sjmprofil.animaltinder.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,6 +10,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        register_button.setOnClickListener { startRegisterActivity() }
+
+    }
+
+    private fun startRegisterActivity() {
+        val intent = RegisterActivity.getIntent(this)
+        startActivity(intent)
     }
 
 }
