567a65c01c621ac9b4c0ab3aee2a3e5abd0bfb46
MGauza <MGauza@hemmersbach.com>
2019-04-18 13:21:51 +0200

    added virables in user

--- app/src/main/java/pl/sjmprofil/animaltinder/models/User.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/models/User.kt
@@ -1,4 +1,10 @@
 package pl.sjmprofil.animaltinder.models
 
-class User {
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val info: String,
+    val imageUrl: String = "http://d3g9pb5nvr3u7.cloudfront.net/authors/539a28913f3c0fd71ed4e43d/2131300937/256.jpg"
+) {
 }
