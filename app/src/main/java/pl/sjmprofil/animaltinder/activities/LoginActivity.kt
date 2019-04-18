7d9ac951ec7a178763978c80be472f1ad3b1a1db
Tomek <Tomasz.Lukaszewski@hemmersbach.com>
2019-04-18 09:24:39 +0200

    commit 2

--- app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
@@ -2,28 +2,12 @@ package pl.sjmprofil.animaltinder.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
 import pl.sjmprofil.animaltinder.R
-import pl.sjmprofil.animaltinder.utilities.Validator
 
 class LoginActivity : AppCompatActivity() {
-
-    private val validator = Validator()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        register_button.setOnClickListener { startRegisterActivity() }
-
-        with(validator) {
-            showEmptyError(login_input_text)
-            showEmptyError(password_input_text)
-        }
-    }
-
-    private fun startRegisterActivity() {
-        val intent = RegisterActivity.getIntent(this)
-        startActivity(intent)
     }
 }
--- app/src/main/java/pl/sjmprofil/animaltinder/activities/RegisterActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/RegisterActivity.kt
@@ -1,40 +1,4 @@
 package pl.sjmprofil.animaltinder.activities
 
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_register.*
-import pl.sjmprofil.animaltinder.R
-import pl.sjmprofil.animaltinder.utilities.Validator
-
-class RegisterActivity : AppCompatActivity() {
-
-    companion object {
-        fun getIntent(context: Context): Intent {
-            return Intent(context, RegisterActivity::class.java)
-        }
-    }
-    private val validator = Validator()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-
-        with(validator) {
-            showNameError(name_input_text)
-            showSurnameError(surname_input_text)
-            showEmailError(email_input_text)
-            showSecondPasswordError(password1_input_text, password2_input_text)
-
-            register_button2.setOnClickListener {
-            if(validateAllFields(name_input_text, surname_input_text, email_input_text, password1_input_text, password2_input_text))
-                Toast.makeText(this@RegisterActivity, "Fields can not be empty!", Toast.LENGTH_SHORT).show()}
-        }
-
-
-
-
-    }
+class RegisterActivity {
 }
