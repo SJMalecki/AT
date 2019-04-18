165dd69c3134f312de9870955b25c220163909b6
Simbiga Michal <MSimbiga@hemmersbach.com>
2019-04-18 16:37:14 +0200

    Add companion object with get intent function

--- app/src/main/java/pl/sjmprofil/animaltinder/activities/MainActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/MainActivity.kt
@@ -1,16 +1,25 @@
 package pl.sjmprofil.animaltinder.activities
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
+
 import pl.sjmprofil.animaltinder.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+
+    companion object {
+        fun getIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
