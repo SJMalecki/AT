01656b06c66f16691e8d071b517f6608c75937ae
Tomek <Tomasz.Lukaszewski@hemmersbach.com>
2019-04-18 10:41:52 +0200

    commit 4

--- app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/LoginActivity.kt
@@ -3,22 +3,27 @@ package pl.sjmprofil.animaltinder.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 import pl.sjmprofil.animaltinder.R
 import pl.sjmprofil.animaltinder.utilities.Validator
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KodeinAware{
 
-    private val validator = Validator()
+    override val kodein by kodein()
+
+    private val validator: Validator by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        register_button.setOnClickListener { startRegisterActivity() }
+        activity_login_register_button.setOnClickListener { startRegisterActivity() }
 
         with(validator) {
-            showEmptyError(login_input_text)
-            showEmptyError(password_input_text)
+            showEmptyError(activity_login_login_input_text)
+            showEmptyError(activity_login_password_input_text)
         }
     }
 
