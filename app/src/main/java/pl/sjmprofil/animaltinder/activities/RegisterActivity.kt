bcd488301177e153f82920fa372a7ff4655d3d3f
Tomek <Tomasz.Lukaszewski@hemmersbach.com>
2019-04-18 10:15:35 +0200

    commit 3

--- app/src/main/java/pl/sjmprofil/animaltinder/activities/RegisterActivity.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/activities/RegisterActivity.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
 import pl.sjmprofil.animaltinder.R
-
+import pl.sjmprofil.animaltinder.utilities.Validator
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,10 +15,24 @@ class RegisterActivity : AppCompatActivity() {
             return Intent(context, RegisterActivity::class.java)
         }
     }
-    
+
+    private val validator = Validator()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        with(validator) {
+            showTextError(name_input_text)
+            showTextError(surname_input_text)
+            showEmailError(email_input_text)
+            showSecondPasswordError(password1_input_text, password2_input_text)
+
+            showEmptyError(password1_input_text)
+            showEmptyError(password2_input_text)
+            showEmptyError(name_input_text)
+            showEmptyError(surname_input_text)
+            showEmptyError(email_input_text)
+        }
     }
 }
