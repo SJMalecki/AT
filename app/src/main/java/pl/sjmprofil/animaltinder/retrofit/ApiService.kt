4c1ccbfaa06604edfaceb18f9ff222989506dd60
MGauza <MGauza@hemmersbach.com>
2019-04-18 14:38:26 +0200

    commented not working code

--- app/src/main/java/pl/sjmprofil/animaltinder/retrofit/ApiService.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/retrofit/ApiService.kt
@@ -9,37 +9,39 @@ import retrofit2.http.*
 
 interface ApiService {
 
+
+// NOT WORKING YET
     // User Queries
-    @Headers("Content-Type: application/json")
-    @GET("users")
-    fun getAllUsers(@Header("Authorization") token: String): Deferred<Response<UsersListDTO>>
-
-    @Headers("Content-Type: application/json")
-    @POST("usercreate")
-    fun createNewUser(@Body user: User): Deferred<Response<UserResponseDTO>>
-
-    @Headers("Content-Type: application/json")
-    @POST("userlogin")
-    fun loginUser(@Body user: User): Deferred<Response<UserResponseDTO>>
-
-    // Advert Queries
-    @Headers("Content-Type: application/json")
-    @POST("advertcreate")
-    fun advertCreate(@Body advert: Advert, @Header("Authorization") token: String): Deferred<Response<UserResponseDTO>>
-
-    @Headers("Content-Type: application/json")
-    @POST("advertfind")
-    fun advertFind(@Body user: User, @Header("Authorization") token: String): Deferred<Response<AdvertResponseDTO>>
-
-    @Headers("Content-Type: application/json")
-    @GET("adverts")
-    fun getAllAdverts(@Header("Authorization") token: String): Deferred<Response<AdvertResponseDTO>>
-
-    // Pictures Queries
-    @Multipart
-    @POST("photoupload")
-    fun uploadUserPhoto(
-        @Part("email") email: String,
-        @Part("photo") photo: MultipartBody.Part
-    ): Deferred<Response<PictureResponseDTO>>
+//    @Headers("Content-Type: application/json")
+//    @GET("users")
+//    fun getAllUsers(@Header("Authorization") token: String): Deferred<Response<UsersListDTO>>
+//
+//    @Headers("Content-Type: application/json")
+//    @POST("usercreate")
+//    fun createNewUser(@Body user: User): Deferred<Response<UserResponseDTO>>
+//
+//    @Headers("Content-Type: application/json")
+//    @POST("userlogin")
+//    fun loginUser(@Body user: User): Deferred<Response<UserResponseDTO>>
+//
+//    // Advert Queries
+//    @Headers("Content-Type: application/json")
+//    @POST("advertcreate")
+//    fun advertCreate(@Body advert: Advert, @Header("Authorization") token: String): Deferred<Response<UserResponseDTO>>
+//
+//    @Headers("Content-Type: application/json")
+//    @POST("advertfind")
+//    fun advertFind(@Body user: User, @Header("Authorization") token: String): Deferred<Response<AdvertResponseDTO>>
+//
+//    @Headers("Content-Type: application/json")
+//    @GET("adverts")
+//    fun getAllAdverts(@Header("Authorization") token: String): Deferred<Response<AdvertResponseDTO>>
+//
+//    // Pictures Queries
+//    @Multipart
+//    @POST("photoupload")
+//    fun uploadUserPhoto(
+//        @Part("email") email: String,
+//        @Part("photo") photo: MultipartBody.Part
+//    ): Deferred<Response<PictureResponseDTO>>
 }
