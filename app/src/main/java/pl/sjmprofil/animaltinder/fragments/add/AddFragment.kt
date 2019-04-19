305f6604751d3263e0cee1adc9c0de96c1998a27
Malecki Sebastian <sebastian.malecki@hemmersbach.com>
2019-04-19 12:15:11 +0200

    Add Bio Dialog Fragment + Functions in Add Fragment

--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/add/AddFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/add/AddFragment.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Parcelable
 import android.provider.MediaStore
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -12,12 +11,14 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.add_fragment_layout.*
 import pl.sjmprofil.animaltinder.R
+import pl.sjmprofil.animaltinder.utilities.DialogFragmentAddBio
 
-class AddFragment: Fragment() {
+class AddFragment : Fragment() {
 
     private val TAKE_PICTURE_BUTTON_REQUEST_ID = 101
     private val OPEN_GALLERY_BUTTON_REQUEST_ID = 102
 
+    private val dialogFragmentAddBio = DialogFragmentAddBio()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.add_fragment_layout, container, false)
@@ -28,34 +29,47 @@ class AddFragment: Fragment() {
 
         setupOpenGalleryButton()
         setupTakePictureButton()
+        setupEditBioButton()
     }
 
     private fun setupTakePictureButton() {
-        button_take_picture_add_fragment.setOnClickListener{
+        button_take_picture_add_fragment.setOnClickListener {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(cameraIntent, TAKE_PICTURE_BUTTON_REQUEST_ID)
         }
     }
 
     private fun setupOpenGalleryButton() {
-        button_open_gallery_add_fragment.setOnClickListener{
-            val galleryIntent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        button_open_gallery_add_fragment.setOnClickListener {
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(galleryIntent, OPEN_GALLERY_BUTTON_REQUEST_ID)
         }
+    }
+
+    private fun setupEditBioButton() {
+        button_edit_bio_add_fragment.setOnClickListener {
+            dialogFragmentAddBio.show(fragmentManager, "DialogFragmentAddBio")
+            dialogFragmentAddBio.onAddButtonClick = { text -> editBioTextView(text) }
+        }
+    }
+
+    private fun editBioTextView(text: String) {
+        text_view_bio_add_fragment.text = text
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == TAKE_PICTURE_BUTTON_REQUEST_ID) {
-            if(resultCode == Activity.RESULT_OK) {
+        if (requestCode == TAKE_PICTURE_BUTTON_REQUEST_ID) {
+            if (resultCode == Activity.RESULT_OK) {
                 val imageHolder = data?.extras?.get("data") as Bitmap
                 image_view_add_fragment.setImageBitmap(imageHolder)
             }
         }
 
-        if(requestCode == OPEN_GALLERY_BUTTON_REQUEST_ID){
-            if(resultCode == Activity.RESULT_OK) {
+        if (requestCode == OPEN_GALLERY_BUTTON_REQUEST_ID) {
+            if (resultCode == Activity.RESULT_OK) {
 
                 val contentURI = data?.data
                 val bitmap = MediaStore.Images.Media.getBitmap(context!!.contentResolver, contentURI)
--- app/src/main/java/pl/sjmprofil/animaltinder/utilities/DialogFragmentAddBio.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/utilities/DialogFragmentAddBio.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.add_bio_dialog_fragment.*
 import pl.sjmprofil.animaltinder.R
 
 class DialogFragmentAddBio : DialogFragment() {
@@ -13,18 +14,18 @@ class DialogFragmentAddBio : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         isCancelable = false
-        return inflater.inflate(R.layout.fragment_my_dialog, container, false)
+        return inflater.inflate(R.layout.add_bio_dialog_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        dialogCancelBtn.setOnClickListener {
+        button_add_bio_dialog_fragment_cancel.setOnClickListener {
             dismiss()
         }
 
-        dialogAddBtn.setOnClickListener {
-            onAddButtonClick?.invoke(dialogEditText.text.toString())
+        button_add_bio_dialog_fragment_add.setOnClickListener {
+            onAddButtonClick?.invoke(text_view_add_bio_dialog_fragment.text.toString())
             dismiss()
         }
     }
