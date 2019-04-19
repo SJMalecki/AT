ae1a7bbc13314db4272138cb46c0e3de4b5507c8
Simbiga Michal <MSimbiga@hemmersbach.com>
2019-04-19 16:47:43 +0200

    asdasdasd

--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/userprofile/UserProfileFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/userprofile/UserProfileFragment.kt
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.coroutines.runBlocking
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.support.kodein
 import org.kodein.di.generic.instance
