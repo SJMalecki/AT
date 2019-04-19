68c5e4c1e8f0b42ccbbb30a02f24419d7e948081
Tomek <Tomasz.Lukaszewski@hemmersbach.com>
2019-04-19 15:31:41 +0200

    commit 1

--- app/src/main/java/pl/sjmprofil/animaltinder/adapters/SwipingViewPagerAdapter.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/adapters/SwipingViewPagerAdapter.kt
@@ -1,4 +1,22 @@
 package pl.sjmprofil.animaltinder.adapters
 
-class SwipingViewPagerAdapter {
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import pl.sjmprofil.animaltinder.fragments.search.SearchFragment
+import pl.sjmprofil.animaltinder.models.Advert
+
+private const val MAX_VALUE = 200
+
+class SwipingViewPagerAdapter(manager: FragmentManager, private val list: List<Advert>) :
+    FragmentStatePagerAdapter(manager) {
+
+
+    override fun getItem(position: Int): Fragment {
+        return SearchFragment()
+    }
+
+    override fun getCount(): Int {
+        return list.size * MAX_VALUE
+    }
 }
--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/search/SearchFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/search/SearchFragment.kt
@@ -1,4 +1,55 @@
 package pl.sjmprofil.animaltinder.fragments.search
 
-class SearchFragment {
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.search_fragment_layout.*
+import pl.sjmprofil.animaltinder.R
+import pl.sjmprofil.animaltinder.databinding.SearchFragmentSlideBinding
+import pl.sjmprofil.animaltinder.adapters.SwipingViewPagerAdapter
+import pl.sjmprofil.animaltinder.models.Advert
+
+
+class SearchFragment : Fragment() {
+
+    private lateinit var viewPagerAdapter: SwipingViewPagerAdapter
+    private lateinit var binding: SearchFragmentSlideBinding
+
+
+    fun getInstance(advert: Advert): SearchFragment {
+        val fragment = SearchFragment()
+        val bundle = Bundle()
+        bundle.putParcelable("advert", advert)
+        fragment.arguments = bundle
+        return fragment
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPagerAdapter()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.search_fragment_slide, container, false)
+        val advert = Advert(
+            0,
+            "email@wp.pl",
+            "Imie",
+            "nazwisko",
+            "https://pbs.twimg.com/profile_images/651092879725740032/5Fau7HaM_400x400.jpg"
+        )
+        binding.advert = advert
+        return binding.root
+    }
+
+    private fun setupViewPagerAdapter() {
+        val adverts = arrayListOf("aaa", "bbb", "ccc")
+        viewPagerAdapter = SwipingViewPagerAdapter(fragmentManager!!, adverts)
+        search_fragment_layout_view_pager.adapter = viewPagerAdapter
+    }
+
+
 }
