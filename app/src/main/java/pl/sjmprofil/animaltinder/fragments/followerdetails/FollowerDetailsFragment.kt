fa21914156ef872e3968125d6034d9459d34ab8c
MGauza <MGauza@hemmersbach.com>
2019-04-19 09:58:50 +0200

    create followers fragment details and layout

--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/followerdetails/FollowerDetailsFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/followerdetails/FollowerDetailsFragment.kt
@@ -1,4 +1,15 @@
 package pl.sjmprofil.animaltinder.fragments.followerdetails
 
-class FollowerDetailsFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pl.sjmprofil.animaltinder.R
+
+class FollowerDetailsFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.follower_details_fragment_layout, container, false)
+    }
 }
