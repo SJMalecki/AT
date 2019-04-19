16b1288b8fc4e2d7a7e7b963765b7ba41c75a4fd
Simbiga Michal <MSimbiga@hemmersbach.com>
2019-04-19 15:06:31 +0200

    Create methods for getting my adverts, creating new adverts, getting all adverts

--- app/src/main/java/pl/sjmprofil/animaltinder/retrofit/ApiService.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/retrofit/ApiService.kt
@@ -28,7 +28,7 @@ interface ApiService {
     fun loginUser(@Body user: User): Deferred<Response<UserResponse>>
 
     @Headers("Content-Type: application/json")
-    @POST("myinfo")
+    @GET("userlogin")
     fun getMyInfo(@Header("Authorization") token: String): Deferred<Response<User>>
 
     //  Advert Queries
@@ -38,7 +38,7 @@ interface ApiService {
 
     @Headers("Content-Type: application/json")
     @POST("advertfind")
-    fun advertFind(@Body user: User, @Header("Authorization") token: String): Deferred<Response<AdvertResponse>>
+    fun getMyAdverts(@Header("Authorization") token: String): Deferred<Response<AdvertResponse>>
 
     @Headers("Content-Type: application/json")
     @GET("adverts")
