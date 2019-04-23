02ec821df26b9367d5b026a2a0869e349a6670ed
Simbiga Michal <MSimbiga@hemmersbach.com>
2019-04-23 10:48:15 +0200

    chenges

--- app/src/main/java/pl/sjmprofil/animaltinder/models/Advert.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/models/Advert.kt
@@ -3,14 +3,11 @@ package pl.sjmprofil.animaltinder.models
 import com.google.gson.annotations.SerializedName
 
 data class Advert(
-    @SerializedName("id")
+    @SerializedName("advert_id")
     var id: Int = 0,
 
     @SerializedName("bio")
     var bio: String = "",
-
-    @SerializedName("email")
-    var email: String = "",
 
     @SerializedName("header")
     var header: String = "",
