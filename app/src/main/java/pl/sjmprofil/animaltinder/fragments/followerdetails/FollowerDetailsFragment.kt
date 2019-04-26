ee297129288d5d8e2776a4d04c6bd62cbbd77f68
MGauza <MGauza@hemmersbach.com>
2019-04-26 13:11:58 +0200

    commit befre pull

--- app/src/main/java/pl/sjmprofil/animaltinder/fragments/followerdetails/FollowerDetailsFragment.kt
+++ app/src/main/java/pl/sjmprofil/animaltinder/fragments/followerdetails/FollowerDetailsFragment.kt
@@ -1,26 +1,51 @@
 package pl.sjmprofil.animaltinder.fragments.followerdetails
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.follower_details_fragment_layout.*
 import pl.sjmprofil.animaltinder.R
 import pl.sjmprofil.animaltinder.databinding.FollowerDetailsFragmentLayoutBinding
+import pl.sjmprofil.animaltinder.models.User
 
 class FollowerDetailsFragment : Fragment() {
 
     lateinit var followerDetailsFragmentLayoutBinding: FollowerDetailsFragmentLayoutBinding
+    lateinit var user: User
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         followerDetailsFragmentLayoutBinding =
             DataBindingUtil.inflate(inflater, R.layout.follower_details_fragment_layout, container, false)
+        setupUserBinding()
+        return followerDetailsFragmentLayoutBinding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        email_button_user_profile_fragment.setOnClickListener {
+            setupEmailActivity()
+        }
+    }
+
+    private fun setupUserBinding() {
         arguments?.let {
             val safeArgUser = FollowerDetailsFragmentArgs.fromBundle(it)
-            followerDetailsFragmentLayoutBinding.user = safeArgUser.user
+            user = safeArgUser.user
+            followerDetailsFragmentLayoutBinding.user = user
         }
-        return followerDetailsFragmentLayoutBinding.root
+    }
+
+    private fun setupEmailActivity() {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(user.email))
+            putExtra(Intent.EXTRA_SUBJECT, "Pet adoption")
+            putExtra(Intent.EXTRA_TEXT, "Hello, I'm interested in adopting your pet!")
+            type = "message/rfc822"
+        }
+        startActivity(Intent.createChooser(intent, "Send email"))
     }
 }
